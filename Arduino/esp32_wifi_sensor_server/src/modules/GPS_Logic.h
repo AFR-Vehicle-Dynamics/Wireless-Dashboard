6ccb38cabf5ec878e86f7392a7782e723c4a4832
stevendoang <alterboi123@gmail.com>
2026-03-23 11:31:33 -0700

    Created GPS_Logic.h

---
+++ Arduino/esp32_wifi_sensor_server/src/modules/GPS_Logic.h
@@ -0,0 +1,56 @@
+#include <Arduino.h>
+#include <TinyGPSPlus.h>
+
+TinyGPSPlus gps;
+
+void GPS_logic() {
+	while (Serial1.available() > 0) {
+		if (gps.encode(Serial1.read())) {
+			Serial.print(F("Location: "));
+			if (gps.location.isValid()) {
+				Serial.print(gps.location.lat(), 6);
+				Serial.print(F(","));
+				Serial.print(gps.location.lng(), 6);
+				Serial.print(F(","));
+				Serial.print(gps.altitude.meters(), 3);
+			} else {
+				Serial.print(F("INVALID"));
+			}
+
+			Serial.print(F("  Date: "));
+			if (gps.date.isValid()) {
+				Serial.print(gps.date.month());
+				Serial.print(F("/"));
+				Serial.print(gps.date.day());
+				Serial.print(F("/"));
+				Serial.print(gps.date.year());
+			} else {
+				Serial.print(F("INVALID"));
+			}
+
+			Serial.print(F("  Time: "));
+			if (gps.time.isValid()) {
+				if (gps.time.hour() < 10) Serial.print(F("0"));
+				Serial.print(gps.time.hour());
+				Serial.print(F(":"));
+				if (gps.time.minute() < 10) Serial.print(F("0"));
+				Serial.print(gps.time.minute());
+				Serial.print(F(":"));
+				if (gps.time.second() < 10) Serial.print(F("0"));
+				Serial.print(gps.time.second());
+				Serial.print(F("."));
+				if (gps.time.centisecond() < 10) Serial.print(F("0"));
+				Serial.print(gps.time.centisecond());
+			} else {
+				Serial.print(F("INVALID"));
+			}
+
+			Serial.println();
+		}
+	}
+
+	if (millis() > 5000 && gps.charsProcessed() < 10) {  //debugging purposes
+		Serial.println(F("GPS not detected."));
+		while (true);
+	}
+}
