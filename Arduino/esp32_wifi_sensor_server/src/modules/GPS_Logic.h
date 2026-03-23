b97c6bd5511ba67151e6bf2c316bad1de3de2c8c
stevendoang <alterboi123@gmail.com>
2026-03-23 12:01:13 -0700

    Updated GPS_Logic.h
    modify as necessary to convert into main code, this is just a guideline of what the GPS will output.

--- Arduino/esp32_wifi_sensor_server/src/modules/GPS_Logic.h
+++ Arduino/esp32_wifi_sensor_server/src/modules/GPS_Logic.h
@@ -3,11 +3,29 @@
 
 TinyGPSPlus gps;
 
-void GPS_logic() {
+/**		GPS data structure.
+		Separate rounding functions may be needed.
+		Uncomment this to use in code or for reference of output types.
+		///////////////////////////////////////////////////////////////
+struct GPS_package{
+	double latitude;
+	double longitude;
+	double altitude;
+
+	uint8_t month;
+	uint8_t day;
+	uint16_t year;
+
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+};
+**/
+void GPS_logic() {	//currently only prints out value, modify to return values if needed
 	while (Serial1.available() > 0) {
-		if (gps.encode(Serial1.read())) {
+		if (gps.encode(Serial1.read())) {	//Serial1 is the default for UART (unless different on esp32)
 			Serial.print(F("Location: "));
-			if (gps.location.isValid()) {
+			if (gps.location.isValid()) {	//outputs Lat/Long/Alt.
 				Serial.print(gps.location.lat(), 6);
 				Serial.print(F(","));
 				Serial.print(gps.location.lng(), 6);
@@ -17,7 +35,7 @@ void GPS_logic() {
 				Serial.print(F("INVALID"));
 			}
 
-			Serial.print(F("  Date: "));
+			Serial.print(F("  Date: "));	//outputs date in MM/DD/YYYY
 			if (gps.date.isValid()) {
 				Serial.print(gps.date.month());
 				Serial.print(F("/"));
@@ -28,7 +46,7 @@ void GPS_logic() {
 				Serial.print(F("INVALID"));
 			}
 
-			Serial.print(F("  Time: "));
+			Serial.print(F("  Time: "));	//outputs time in HH:MM:SS
 			if (gps.time.isValid()) {
 				if (gps.time.hour() < 10) Serial.print(F("0"));
 				Serial.print(gps.time.hour());
@@ -38,9 +56,6 @@ void GPS_logic() {
 				Serial.print(F(":"));
 				if (gps.time.second() < 10) Serial.print(F("0"));
 				Serial.print(gps.time.second());
-				Serial.print(F("."));
-				if (gps.time.centisecond() < 10) Serial.print(F("0"));
-				Serial.print(gps.time.centisecond());
 			} else {
 				Serial.print(F("INVALID"));
 			}
